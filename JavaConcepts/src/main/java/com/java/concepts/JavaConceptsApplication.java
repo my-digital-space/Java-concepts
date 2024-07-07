4d6fcce6609c7654f36e2d19131db51669904799
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-07-07 17:06:07 +0530

    initial commit

---
+++ JavaConcepts/src/main/java/com/java/concepts/JavaConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.java.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConceptsApplication.class, args);
+	}
+
+}
