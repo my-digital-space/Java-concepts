c7f6d26f66a2e5e3a339dc344eeb7d426544aa22
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-10-04 20:04:24 +0530

    Update WhyOptional.java

--- Optional/src/main/java/org/demo/optional/WhyOptional.java
+++ Optional/src/main/java/org/demo/optional/WhyOptional.java
@@ -1,20 +1,52 @@
 package org.demo.optional;
 
 import org.demo.util.Employee;
+
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public class WhyOptional {
 
     public static void main(String[] args) {
-        Employee employee = findEmployeeById(1);
-        System.out.println("Employee Name: " + employee.getEmpName());
+        int id = 1;
+//        Employee employee = findEmployeeById(id);
+//        if (employee != null) {
+//            System.out.println("Employee Name: " + employee.getEmpName());
+//        } else {
+//            System.out.println("Employee not found with id: " + id);
+//        }
+
+//        Optional<Employee> optionalEmployee = findEmployeeByIdOptional(1);
+//        if (optionalEmployee.isPresent()){
+//            System.out.println(optionalEmployee.get());
+//        } else {
+//            System.out.println("Employee not found with id: " + id);
+//        }
+
+//        Optional<Employee> optionalEmployee = findEmployeeByIdOptional(1);
+//        Employee defaultEmployee = new Employee(
+//                0, "Default name", "Default Department",
+//                BigDecimal.valueOf(100.00), 30);
+//        System.out.println(optionalEmployee.orElse(defaultEmployee));
+
+        Optional<Employee> optionalEmployee = findEmployeeByIdOptional(1);
+        System.out.println(optionalEmployee.map(Employee::getEmpName).orElse("My Default Name"));
+
+
+    }
+
+    public static Optional<Employee> findEmployeeByIdOptional(int id) {
+        Employee employee = new Employee(
+                1, "Name 1", "HR Department",
+                BigDecimal.valueOf(100.00), 30);
+        return Optional.ofNullable(employee);
     }
 
     public static Employee findEmployeeById(int id) {
         Employee employee = new Employee(
                 1, "Name 1", "HR Department",
                 BigDecimal.valueOf(100.00), 30);
-        return employee;
+        return null;
     }
 
 }
