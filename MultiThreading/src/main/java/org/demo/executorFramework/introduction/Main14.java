d25a835b82710a59bba466f886fa7e319aefd883
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-10-13 20:27:36 +0530

    ExecutorService.newCachedThreadPool()

---
+++ MultiThreading/src/main/java/org/demo/executorFramework/introduction/Main14.java
@@ -0,0 +1,16 @@
+package org.demo.executorFramework.introduction;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Main14 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService schedulerService = Executors.newCachedThreadPool();
+
+
+    }
+}
