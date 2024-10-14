fc824413c1bc67f0e44857783f53b0226d326731
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-10-14 12:17:31 +0530

    initial commit

---
+++ CollectionsFramework/src/main/java/org/demo/collection/introduction/Main1.java
@@ -0,0 +1,10 @@
+package org.demo.collection.introduction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main1 {
+    public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+    }
+}
