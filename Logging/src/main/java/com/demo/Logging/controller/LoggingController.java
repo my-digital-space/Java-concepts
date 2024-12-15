c1510afa37c0f56da248f81c863f869b96d81977
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-12-15 19:28:10 +0530

    LoggingSystem

--- Logging/src/main/java/com/demo/Logging/controller/LoggingController.java
+++ Logging/src/main/java/com/demo/Logging/controller/LoggingController.java
@@ -3,9 +3,14 @@ package com.demo.Logging.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.logging.LoggingSystem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoggingController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @Autowired
+    private LoggingSystem loggingSystem;
+
+    @PostMapping("changeLogLevel/{loggerName}/{level}")
+    public String setLogLevel(@PathVariable String loggerName, @PathVariable String level) {
+        try {
+            loggingSystem.setLogLevel(loggerName, LogLevel.valueOf(level.toUpperCase()));
+            return "Log level for " + loggerName + " set to " + level.toUpperCase();
+        } catch (IllegalArgumentException e) {
+            return "Invalid log level: " + level;
+        }
+    }
 
     @GetMapping("logLevels")
     public ResponseEntity<String> printLogLevels() {
@@ -37,5 +55,7 @@ public class LoggingController {
 
         return new ResponseEntity<>("Logging Successful!", HttpStatus.OK);
     }
+
+
 
 }
