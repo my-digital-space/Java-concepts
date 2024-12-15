adf766499986f1324ef9bad392cff9bcbd153e32
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-12-15 13:41:04 +0530

    @Slf4j

--- Logging/src/main/java/com/demo/Logging/controller/LoggingController.java
+++ Logging/src/main/java/com/demo/Logging/controller/LoggingController.java
@@ -1,5 +1,6 @@
 package com.demo.Logging.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/log/v1/")
+//@Slf4j
 public class LoggingController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
