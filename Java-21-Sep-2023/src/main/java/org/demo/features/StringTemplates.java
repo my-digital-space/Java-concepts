4505971d11b9b78d96edbf24113df64ccb67f859
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-01-18 17:13:53 +0530

    StringTemplates

---
+++ Java-21-Sep-2023/src/main/java/org/demo/features/StringTemplates.java
@@ -0,0 +1,16 @@
+package org.demo.features;
+
+import static java.lang.StringTemplate.STR;
+
+public class StringTemplates {
+    public static void main(String[] args) {
+        String name = "Rahul";
+        int age = 28;
+
+        // Using a string template
+        String message = STR."Hello, \{name}. You are \{age} years old.";
+        System.out.println(message);
+    }
+}
+
+
