df77f008a3777888073688cf5da9381667cf76a1
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-01-18 17:37:29 +0530

    demo

---
+++ Java-8-March-2014/src/main/java/org/demo/misc/Demo1.java
@@ -0,0 +1,13 @@
+package org.demo.misc;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        //System.out.println(list.getLast());
+    }
+
+}
