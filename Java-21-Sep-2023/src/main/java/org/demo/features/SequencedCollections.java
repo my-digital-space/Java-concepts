89a9291a05730677aa7ef5e0042e585aaf02e2c5
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-01-18 17:51:25 +0530

    Update SequencedCollections.java

--- Java-21-Sep-2023/src/main/java/org/demo/features/SequencedCollections.java
+++ Java-21-Sep-2023/src/main/java/org/demo/features/SequencedCollections.java
@@ -5,7 +5,9 @@ public class SequencedCollections {
 
     public static void main(String[] args) {
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(list.getFirst());
         System.out.println(list.getLast());
+        System.out.println(list.reversed());
     }
 
 }
