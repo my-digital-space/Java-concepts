a40ba619338020fb8d510bae68c0282beda6c4e1
prosenjit005 <mydigitalspace.pro@gmail.com>
2025-01-18 17:30:23 +0530

    Create SequencedCollections.java

---
+++ Java-21-Sep-2023/src/main/java/org/demo/features/SequencedCollections.java
@@ -0,0 +1,11 @@
+package org.demo.features;
+import java.util.List;
+
+public class SequencedCollections {
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println(list.getLast());
+    }
+
+}
